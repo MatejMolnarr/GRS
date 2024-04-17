00f573a3e8120c118ac0213d4dee73b691b2a06b
Matej Molnár <167308368+MatejMolnarr@users.noreply.github.com>
2024-04-17 12:49:36 +0200

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float a,b,c;
+printf("zadaj cislo a:");
+scanf("%f", &a);
+
+printf("zadaj cislo b:");
+scanf("%f", &b);
+
+c=a/b;
+printf("%f", c);
+
+return 0;
+} 
